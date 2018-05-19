453a884e4ca09c05899bba9de1d692ce417d8a62
random <lordsimplexx88@gmail.com>
2018-05-19 12:12:53 +0200

    + Commit

---
+++ src/main/java/de/xenithbyte/minigame/gamestates/GameState.java
@@ -0,0 +1,11 @@
+package de.xenithbyte.minigame.gamestates;
+
+public abstract class GameState {
+
+    public static final int LOBBY_STATE =0,
+                            INGAME_STATE = 1,
+                            ENDING_STATE = 2;
+
+    public abstract void start();
+    public abstract void stop();
+}
