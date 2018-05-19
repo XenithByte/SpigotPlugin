f59a397f4916b9b0fae6fe28917ef5bea5926700
random <lordsimplexx88@gmail.com>
2018-05-19 12:37:54 +0200

    + Commit

--- src/main/java/de/xenithbyte/minigame/gamestates/GameStateManager.java
+++ src/main/java/de/xenithbyte/minigame/gamestates/GameStateManager.java
@@ -3,5 +3,24 @@ package de.xenithbyte.minigame.gamestates;
 public class GameStateManager {
 
     private GameState[] gameStates = new GameState[3];
-    
+    private GameState currentGameState;
+
+    public GameStateManager() {
+        gameStates[GameState.LOBBY_STATE] = new LobbyState();
+        gameStates[GameState.INGAME_STATE] = new IngameState();
+        gameStates[GameState.ENDING_STATE] = new EndingState();
+    }
+
+    public void setGameState(int gameStateIndex) {
+        if(currentGameState != null)
+            currentGameState.stop();
+        currentGameState = gameStates[gameStateIndex];
+        currentGameState.start();
+    }
+
+    public void stopCurrentGameState() {
+        currentGameState.stop();
+        currentGameState = null;
+    }
+
 }
--- src/main/java/de/xenithbyte/minigame/main/Main.java
+++ src/main/java/de/xenithbyte/minigame/main/Main.java
@@ -1,5 +1,6 @@
 package de.xenithbyte.minigame.main;
 
+import de.xenithbyte.minigame.gamestates.GameState;
 import de.xenithbyte.minigame.gamestates.GameStateManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -7,15 +8,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 
     private static Main plugin;
-    private static String prefix = "[MiniGame] ";
+    private static String prefix = "§8[§eMiniGame§8]§7 ";
 
     private static GameStateManager gameStateManager;
 
     public void onEnable() {
         plugin = this;
         gameStateManager = new GameStateManager();
+        gameStateManager.setGameState(GameState.LOBBY_STATE);
 
-        Bukkit.getConsoleSender().sendMessage(prefix + "");
+        Bukkit.getConsoleSender().sendMessage(prefix + "Das Plugin ist gestartet.");
     }
 
     public static Main getPlugin() {
